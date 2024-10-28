662d455f0038874e5ee79dbfbf07851b3300d7d7
xing142857 <xing142857@gmail.com>
2024-10-28 17:20:06 +1100

    link Navigation Bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -12,9 +12,11 @@ const Navigation = () => {
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/find-us">FindUs</Nav.Link>
+            <Nav.Link href="/gallery">Gallery</Nav.Link>
+            <Nav.Link href="/about-us">About</Nav.Link>
+            <Nav.Link href="/contact-us">Contact</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
