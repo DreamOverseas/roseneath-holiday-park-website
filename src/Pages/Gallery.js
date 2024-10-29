d5be35b85c49cf4b09418f98059fdea25d2123fd
xing142857 <xing142857@gmail.com>
2024-10-29 14:23:54 +1100

    Add basic gallery

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,15 +1,33 @@
 import React from "react";
 import "../Css/Gallery.css";
+import { Container, Row, Col, Image } from 'react-bootstrap';
 
 const Gallery = () => {
 
-    // TODO
+    const images = [
+        "/GalleryImage/DSC02707.jpg",
+        "/GalleryImage/DSC02711.jpg",
+        "/GalleryImage/DSC02714.jpg",
+        "/GalleryImage/DSC02721.jpg",
+        "/GalleryImage/DSC02724.jpg",
+        "/GalleryImage/DSC02730.jpg",
+        "/GalleryImage/DSC02740.jpg",
+        "/GalleryImage/DSC02748.jpg",
+        "/GalleryImage/DSC02751.jpg",
+    ];
 
     return (
-        <div>
-            <p> This is Gallery Page </p>
-        </div>
-    );
+        <Container fluid>
+          <h2 className="text-center my-4">Image Gallery</h2>
+          <Row>
+            {images.map((src, index) => (
+              <Col key={index} xs={6} md={4} lg={3} className="mb-4">
+                <Image src={src} thumbnail fluid/>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      );
 };
 
 export default Gallery;
