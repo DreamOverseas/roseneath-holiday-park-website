d5cca492576ca8981464f2d5685059b013f0e960
xing142857 <xing142857@gmail.com>
2024-10-29 13:59:40 +1100

    Improve NavWord position

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -15,21 +15,26 @@ const Navigation = () => {
     return (
       <Navbar bg="light" data-bs-theme="light" className="NavigationBar sticky-top">
         <Container>
+
           <Navbar.Brand href="/">
             <Figure.Image
               width={100}
               src="/logo192.png"
             />
           </Navbar.Brand>
-          <Nav className="me-auto">
+
+          <Nav>
             <Nav.Link className="NavWord" href="/">{t('Home')}</Nav.Link>
             <Nav.Link className="NavWord" href="/find-us">{t('Find us')}</Nav.Link>
             <Nav.Link className="NavWord" href="/gallery">{t('Gallery')}</Nav.Link>
             <Nav.Link className="NavWord" href="/about-us">{t('About')}</Nav.Link>
             <Nav.Link className="NavWord" href="/contact-us">{t('Contact')}</Nav.Link>
           </Nav>
-          <Nav.Link className="NavWord" href="/contact-us">+61 (03) 5157-8298</Nav.Link>
-          <Button className="NavWord">{t('Contact us')}</Button>
+
+          <div className="NavContact">
+            <Nav.Link className="NavWord" href="/contact-us">+61 (03) 5157-8298</Nav.Link>
+            <Button className="NavWord">{t('Contact us')}</Button>
+          </div>
         </Container>
       </Navbar>
     );
