5a14b87c33b9f4e1489c084ffdd440314ddfce09
xing142857 <xing142857@gmail.com>
2024-10-29 16:14:47 +1100

    Basic function of Gallery finished

--- src/Pages/Gallery.js
+++ src/Pages/Gallery.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "../Css/Gallery.css";
-import { Container, Row, Col, Image } from 'react-bootstrap';
+import { Container, Row, Col, Image, Carousel } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 const Gallery = () => {
+
+    const { t } = useTranslation();
 
     const images = [
         "/GalleryImage/DSC02707.jpg",
@@ -14,20 +17,44 @@ const Gallery = () => {
         "/GalleryImage/DSC02740.jpg",
         "/GalleryImage/DSC02748.jpg",
         "/GalleryImage/DSC02751.jpg",
+        "/GalleryImage/DSC02800.jpg",
+        "/GalleryImage/DSC02814.jpg",
+        "/GalleryImage/DSC02855.jpg",
+        "/GalleryImage/DSC02868.jpg",
+        "/GalleryImage/DSC02872.jpg",
+        "/GalleryImage/DSC02879.jpg",
+        "/GalleryImage/DSC02900.jpg",
+        "/GalleryImage/DSC02923.jpg",
+        "/GalleryImage/DSC02925.jpg",
+        "/GalleryImage/DSC02934.jpg",
+        "/GalleryImage/DSC03083.jpg",
     ];
 
     return (
-        <Container fluid>
-          <h2 className="text-center my-4">Image Gallery</h2>
-          <Row>
-            {images.map((src, index) => (
-              <Col key={index} xs={6} md={4} lg={3} className="mb-4">
-                <Image src={src} thumbnail fluid/>
-              </Col>
-            ))}
-          </Row>
-        </Container>
-      );
-};
+        <>
+            <Carousel className="text-center">
+                <Carousel.Item>
+                    <Image className="slideImage" src="/GalleryImage/DSC02707.jpg" thumbnail/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Image className="slideImage" src="/GalleryImage/DSC02800.jpg" thumbnail/>
+                </Carousel.Item>
+                <Carousel.Item>
+                    <Image className="slideImage" src="/GalleryImage/DSC03083.jpg" thumbnail/>
+                </Carousel.Item>
+            </Carousel>
 
+            <Container fluid>
+            <h2 className="text-center my-4">{t('Image Gallery')}</h2>
+            <Row>
+                {images.map((src, index) => (
+                <Col key={index} xs={6} md={6} lg={4} className="mb-4">
+                    <Image className="galleryImage" src={src} thumbnail fluid />
+                </Col>
+                ))}
+            </Row>
+            </Container>
+        </>
+      );
+    }
 export default Gallery;
