41e013c1c413d3a32d61a9216d2a0e98f7f48b89
xing142857 <xing142857@gmail.com>
2024-10-29 11:23:44 +1100

    Finish the board of the Navigation Bar

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -1,16 +1,21 @@
 import React from "react";
 import "../Css/Navigation.css";
 import Container from 'react-bootstrap/Container';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Figure} from 'react-bootstrap';
 
 const Navigation = () => {
 
     // TODO
 
     return (
-      <Navbar bg="light" data-bs-theme="light">
+      <Navbar bg="light" data-bs-theme="light" className="NavigationBar fixed-top shadow p-3 mb-5 bg-white">
         <Container>
-          <Navbar.Brand href="/">Navbar</Navbar.Brand>
+          <Navbar.Brand href="/">
+            <Figure.Image
+              width={100}
+              src="/logo192.png"
+            />
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/find-us">FindUs</Nav.Link>
