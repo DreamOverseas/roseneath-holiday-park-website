3029827294c0407c59624da17171a206e9126036
Hanny <zyhaierciyuan@gmail.com>
2024-11-14 16:13:31 +1100

    Added Company Details into the Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -18,7 +18,7 @@ const Footer = () => {
         <Row className='footer-content'>
           <Col
             xs={12}
-            md={4}
+            md={2}
             className='footer-logo-section d-flex flex-column align-items-center mb-3 mb-md-0'
           >
             <Image src='logo192.png' className='footer-logo' />
@@ -35,10 +35,22 @@ const Footer = () => {
             <p>Telephone: +61 (03) 5157-8298</p>
             <p>Email: info@roseneathholidaypark.au</p>
             <p>Address: 422 Woodpile Rd, Meerlieu VIC 3862, Australia</p>
-          </Col>
+          </Col>          
           <Col
             xs={12}
             md={4}
+            className='footer-aboutus-info text-center text-md-left mb-3 mb-md-0'
+          >
+            <h5 className='footer-aboutus-title'>Roseneath Holiday Park Management Pty Ltd</h5>
+            <p>ACN: 679 085 477 </p>
+            <p>ABN: 52 679 085 477 </p>
+            <p>Bank name: CBA </p>
+            <p>BSB: 0630182 </p>
+            <p>Account Number: 1177 8453 </p>
+          </Col>
+          <Col
+            xs={12}
+            md={2}
             className='footer-button-section d-flex justify-content-center justify-content-md-end align-items-center'
           >
             <Button className='footer-button' onClick={jumpToContact}>
