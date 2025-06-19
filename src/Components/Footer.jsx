4f005999cd759ef866a1540e8bb1fd5a395f4747
xing142857 <xing142857@gmail.com>
2025-06-19 14:50:05 +1000

    Update contact information

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -36,77 +36,33 @@ const Footer = () => {
             md={3}
             className='footer-aboutus-info text-center text-md-left mb-3 mb-md-0'>
             <Row className="contact-info-row">
-                <p>
-                    <b>{t("contact_individual_title")}</b> &#9978;
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-pin-angle"></i> &nbsp;
-                    {t("contact_individual_location")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-telephone-inbound"></i> &nbsp;
-                    {t("contact_individual_land")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-person"></i> &nbsp;
-                    {t("contact_individual_name")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-telephone-inbound"></i> &nbsp;
-                    {t("contact_individual_phone")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-mailbox"></i> &nbsp;
-                    {t("contact_individual_email")}
-                </p>
+              <div>
+                  <p><b>{t("contact_individual_title")}</b> &#9978;</p>
+                  <p><b>{t("contact_individual_subject")}</b></p>
+                  
+                  <p><i className="bi bi-pin-angle"></i> &nbsp; {t("contact_individual_location")}</p>
+                  <p><i className="bi bi-telephone-inbound"></i> &nbsp; {t("contact_individual_land")}</p>
+                  <p><i className="bi bi-person"></i> &nbsp; {t("contact_individual_name")}</p>
+                  <p><i className="bi bi-telephone-inbound"></i> &nbsp; {t("contact_individual_phone")}</p>
+                  <p><i className="bi bi-person"></i> &nbsp; {t("contact_group_name")}</p>
+                  <p><i className="bi bi-telephone-inbound"></i> &nbsp; {t("contact_group_phone")}</p>
+                  <p><i className="bi bi-mailbox"></i> &nbsp; {t("contact_individual_email")}</p>
+              </div>
             </Row>
           </Col>
           <Col xs={12}
             md={3}
             className='footer-aboutus-info text-center text-md-left mb-3 mb-md-0'>
             <Row className="contact-info-row">
-                <p>
-                    <b>{t("contact_group_title")}</b> &#128188;
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <b>{t("contact_group_subject")}</b>
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-pin-angle"></i> &nbsp;
-                    {t("contact_group_location")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-person"></i> &nbsp;
-                    {t("contact_group_name")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-telephone-inbound"></i> &nbsp;
-                    {t("contact_group_phone")}
-                </p>
-            </Row>
-            <Row className="contact-info-row">
-                <p>
-                    <i class="bi bi-mailbox"></i> &nbsp;
-                    {t("contact_group_email")}
-                </p>
+              <div>
+                  <p><b>{t("contact_group_title")}</b> &#128188;</p>
+                  <p><b>{t("contact_group_subject")}</b></p>
+                  
+                  <p><i className="bi bi-pin-angle"></i> &nbsp; {t("contact_group_location")}</p>
+                  <p><i className="bi bi-person"></i> &nbsp; {t("contact_group_name")}</p>
+                  <p><i className="bi bi-telephone-inbound"></i> &nbsp; {t("contact_group_phone")}</p>
+                  <p><i className="bi bi-mailbox"></i> &nbsp; {t("contact_group_email")}</p>
+              </div>
             </Row>
           </Col>        
           <Col
